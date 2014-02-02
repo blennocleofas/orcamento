8c6c3a358e057b69592c760aaceb7a6ed6ecee63
Blenno Cleofas <blennopardim@gmail.com>
2014-02-01 22:19:52 -0200

    Update aeroportos.rb

--- app/models/aeroportos.rb
+++ app/models/aeroportos.rb
@@ -2,23 +2,5 @@ class Aeroportos < ActiveRecord::Base
 	include Tire::Model::Search
 	include Tire::Model::Callbacks
 
-	settings :number_of_shards => 1,
-	            :number_of_replicas => 1,
-	            :analysis => {
-	                :filter => {
-	                  :text_ngram  => {
-	                    "type"     => "nGram",
-	                    "max_gram" => 10,
-	                    "min_gram" => 2 }
-	                },
-	                :analyzer => {
-	                  :busca_aeroportos => {
-	                     "tokenizer"    => "standard",
-	                     "filter"       => ["standard", "lowercase", "stop", "kstem", "ngram", "text_ngram"],
-	                     "type"         => "custom" }
 
-	                }
-	              } do
-	       mapping { indexes :nome, :type => 'string', :analyzer => "busca_aeroportos" }
-	end
 end
