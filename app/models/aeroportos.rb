785521c83b1b9c17111c3043d4f4f4efd5ad312a
Blenno <blenno@live.com.br>
2014-02-06 09:43:32 -0200

    GEO localização

--- app/models/aeroportos.rb
+++ app/models/aeroportos.rb
@@ -2,5 +2,16 @@ class Aeroportos < ActiveRecord::Base
 	include Tire::Model::Search
 	include Tire::Model::Callbacks
 
+	tire do
+	    mapping do
+	      indexes :name,       type: 'string', as: 'id'
+	      indexes :cod,        type: 'string', as: 'cod'
+	      indexes :location,   type: 'geo_point', as: 'lat_lon'
+	    end
+	  end
+
+  def lat_lon
+    [lat.to_f, lon.to_f].join(',')
+  end
 
 end
