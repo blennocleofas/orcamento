bf28204d7c15e518457fdd4990b1e494a2f156e1
Blenno <blenno@live.com.br>
2014-02-25 15:39:51 -0300

    Enviando Alterações

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140201033026) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "aereos", force: true do |t|
     t.string   "voo"
     t.string   "cia"
