61906845e42bb14110752f36f21d7f518b4a7fed
Blenno <blenno@live.com>
2014-03-14 08:27:47 -0300

    Melhorias feitas em Casa

--- app/assets/javascripts/height.js
+++ app/assets/javascripts/height.js
@@ -66,17 +66,49 @@ $(document).ready(function(){
 });
 
 
-$(document).ready(function(){
-    
-    $('.section').bind('scroll',chk_scroll);
+
+// Cache selectors
+var lastId,
+    topMenu = $("ul.li"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 300);
+  e.preventDefault();
 });
 
-function chk_scroll(e)
-{
-    var elem = $(e.currentTarget);
-    if (elem[0].scrollHeight - elem.scrollTop() == elem.outerHeight())
-    {
-        console.log("bottom");
-    }
-
-}
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#"+id+"]").parent().addClass("active");
+   }                   
+});
--- app/assets/javascripts/mkt.js
+++ app/assets/javascripts/mkt.js
@@ -1,4 +1,9 @@
+//= require jquery
+//= require jquery_ujs
+//= require jquery.ui.all
+//= require foundation
 //= require countdown.min
 //= require jquery-ui-scrollable
 //= require jquery.windows
 //= require height
+$(function(){ $(document).foundation(); });
